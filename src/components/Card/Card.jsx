5cafc3fec5409dc3d01d18ad9a3c8814a0ab71df
ernesto <ernestofamorim@gmail.com>
2024-02-22 23:58:19 -0300

    card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,14 @@
-import styles from "Card.module.css"
+import qrCode from '../../assets/image-qr-code.png';
+import "./Card.css"
 
 function Card() {
     return (
         <>
 
-            <div>
-
+            <div className="card-container">
+                <img src={qrCode}></img>
+                <p>Improve your front-end skill by building projects</p>
+                <small>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</small>
             </div>
 
         </>
